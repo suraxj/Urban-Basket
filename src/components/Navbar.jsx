c2297601b6f1a7dab33f3deb9597b37705aa686d
suraxj <prakashsinghsuraj69@gmail.com>
2025-05-18 09:55:29 +0530

    initial

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const logout = async ()=>{
     
         <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
 
-            <NavLink to='/'>
+            <NavLink to='/'onClick={()=> setOpen(false)} >
                 <img className="h-9" src={assets.logo} alt="Logo" />
             </NavLink>
 
@@ -29,10 +29,8 @@ const logout = async ()=>{
                     <img src={assets.search_icon} alt="search classname= 'w-4 h-4' " />
                 </div>
 
-                <div className="relative cursor-pointer">
-                    <svg width="18" height="18" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M.583.583h2.333l1.564 7.81a1.17 1.17 0 0 0 1.166.94h5.67a1.17 1.17 0 0 0 1.167-.94l.933-4.893H3.5m2.333 8.75a.583.583 0 1 1-1.167 0 .583.583 0 0 1 1.167 0m6.417 0a.583.583 0 1 1-1.167 0 .583.583 0 0 1 1.167 0" stroke="#615fff" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
+                <div onClick={()=> navigate('/cart')} className="relative cursor-pointer">
+                    <img src={assets.nav_cart_icon} alt="cart" className='w-6 opacity-80' />
                     <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">3</button>
                 </div>
 
@@ -41,11 +39,12 @@ const logout = async ()=>{
                 </button> )
                 :
                 (
-                    <div>
+                    <div className='relative group'>
                         <img src={assets.profile_icon} className='w-10' alt="" />
-                        <ul>
-                            <li>My Orders</li>
-                            <li>Logout</li>
+                        <ul className='hidden group-hover:block absolute top-10 right-0 bg-white shadow
+                         border-gray-200 py-2.5 w-30 rounded-md text-sm z-40 '>
+                            <li onClick={()=> navigate('my-orders')} className='p-1.5 pl-3 hover:bg-primary/10 cursor-pointer'>My Orders</li>
+                            <li onClick={logout} className='p-1.5 pl-3 hover:bg-primary/10 cursor-pointer'>Logout</li>
 
                         </ul>
                     </div>
--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,15 +1,69 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { dummyProducts } from "../assets/assets";
+import toast from "react-hot-toast";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => { 
+
+  const currency = import.meta.VITE_CURRENCY;
+
   const navigate = useNavigate();
   const [user, setUser] = useState(true);
   const [isSeller, setIsSeller] = useState(false); // ✅ Fixed camelCase convention
   const [showUserLogin, setshowUserLogin] = useState(false);
+  const [products, setProducts] = useState([])
+  const [cartItems, setCartItems] = useState({})
 
-  const value = { navigate, user, setUser, isSeller, setIsSeller, setshowUserLogin, showUserLogin }; // ✅ Match naming
+  // Fetch All Products
+  const fetchProducts = async ()=>{
+    setProducts(dummyProducts)
+  }
+
+  //Add Products to Cart
+const addToCart = ()=>{
+  let cartData = structuredClone(cartItems);
+
+  if(cartData[itemId]){
+    cartData[itemId] += 1;
+  }else{
+    cartData[itemId] = 1;
+  }
+  setCartItems(cartData);
+  toast.success("Added To Cart")
+}
+
+// Update Cart Item Quantity
+const updateCartItem = (itemId, quantity)=>{
+  let cartData = structuredClone(cartItems);
+  cartData[itemId] = quantity;
+  setCartItems(cartData)
+  toast.success("Cart Updated")
+
+}
+
+// Remove Product From Cart 
+const removeFromCart = (itemId)=>{
+  let cartData = structuredClone(cartItems);
+  if(cartData[itemId]){
+    cartData[itemId] -= 1;
+    if(cartData[itemId] ===0){
+    delete cartData[itemId];
+  }
+
+}
+toast.success("Removed From Cart")
+setCartItems(cartData)
+}
+
+
+  useEffect(()=>{
+      fetchProducts()
+  },[])
+
+  const value = { navigate, user, setUser, isSeller, setIsSeller, setshowUserLogin, 
+    showUserLogin, products, currency, addToCart, updateCartItem, removeFromCart, cartItems }; // ✅ Match naming
 
   return (
     <AppContext.Provider value={value}>
